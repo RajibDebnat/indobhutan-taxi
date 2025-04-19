1f62daf5b7bf283f5bd75c059becf8dbce30c689
Rajib Debnath <rajibdebnath923@gmail.com>
2025-04-19 23:18:53 +0530

    added new section

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import HeroSection from "@/components/Home";
 export default function Home() {
   return (
-  <main  >
+  <main  className=" bg-white" >
 
 <HeroSection />
 <div className="px-24  max-xl:px-12 max-md:px-10 max-sm:px-5 pt-0">
